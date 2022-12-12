927e27d0f29e8ee6d80bb12856c7963ef72880e6
jeevan-kishore <kishor080@gmail.com>
2022-12-12 12:55:05 +0530

    fix(decode): remove decode uri component

--- node-atob.js
+++ node-atob.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function atob(str) {
-  return decodeURIComponent(escape(Buffer.from(str, 'base64').toString('utf-8')));
+  return Buffer.from(str, 'base64').toString('utf-8');
 }
 
 module.exports = atob.atob = atob;
